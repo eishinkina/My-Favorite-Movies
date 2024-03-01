0fa2486988db2cc1a070703476b3e13e90e864d9
Ekaterina Ishinkina <esiforlearn@gmail.com>
2024-03-01 15:49:14 +0400

    fix

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,33 +1,6 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
 
-// export const useMovieStore = defineStore("movieStore", {
-//   state: () => ({
-//     movies: [],
-//     activeTab: 2,
-//   }),
-//   getters: {
-//     watchedMovies() {
-//       return this.movies.filter((el) => el.isWatched === true);
-//     },
-//     totalCountMovies() {
-//       return this.movies.length;
-//     },
-//   },
-//   actions: {
-//     setActiveTab(id) {
-//       this.activeTab = id;
-//     },
-//     toggleWatched(id) {
-//       const idx = this.movies.findIndex((el) => el.id === id);
-//       this.movies[idx].isWatched = !this.movies[idx].isWatched;
-//     },
-//     deleteMovie(id) {
-//       this.movies = this.movies.filter((el) => el.id !== id);
-//     },
-//   },
-// });
-
 export const useMovieStore = defineStore("movieStore", () => {
   const movies = ref([]);
   const activeTab = ref(2);
