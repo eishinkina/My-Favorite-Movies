564e91800bb3a0b97704df3724a42ec5be92437a
Ekaterina Ishinkina <esiforlearn@gmail.com>
2024-03-01 16:25:20 +0400

    saved changes when reloading the page

--- src/stores/MovieStore.js
+++ src/stores/MovieStore.js
@@ -1,19 +1,26 @@
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 
 export const useMovieStore = defineStore("movieStore", () => {
   const movies = ref([]);
   const activeTab = ref(2);
+
+  const moviesOnLocalStorage = localStorage.getItem("movies");
+  if (moviesOnLocalStorage) {
+    movies.value = JSON.parse(moviesOnLocalStorage);
+    console.log(JSON.parse(moviesOnLocalStorage));
+  }
+
   const setActiveTab = (id) => {
     activeTab.value = id;
   };
 
   const watchedMovies = computed(() => {
-    movies.value.filter((el) => el.isWatched === true);
+    return movies.value.filter((el) => el.isWatched === true);
   });
 
   const totalCountMovies = computed(() => {
-    movies.value.length;
+    return movies.value.length;
   });
 
   const toggleWatched = (id) => {
@@ -24,6 +31,16 @@ export const useMovieStore = defineStore("movieStore", () => {
   const deleteMovie = (id) => {
     movies.value = movies.value.filter((el) => el.id !== id);
   };
+
+  watch(
+    () => movies.value,
+    (newMovies) => {
+      localStorage.setItem("movies", JSON.stringify(newMovies));
+      console.log("Saved to localStorage", newMovies);
+    },
+    { deep: true }
+  );
+
   return {
     movies,
     activeTab,
